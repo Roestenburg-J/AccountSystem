7bd2568618de657aafa5b843a1d00bdf5dfd6e47
Janus Roestenburg <JanusRoestenburg@gmail.com>
2021-09-28 10:05:06 +0200

    Updated Type Control

--- AccountSystemWeb/src/main/java/za/ac/nwu/ac/web/sb/controller/AccountTypeController.java
+++ AccountSystemWeb/src/main/java/za/ac/nwu/ac/web/sb/controller/AccountTypeController.java
@@ -3,22 +3,27 @@ package za.ac.nwu.ac.web.sb.controller;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import za.ac.nwu.ac.domain.service.GeneralResponse;
 
 @RestController
-@RequestMapping("za.ac.nwu.ac.web.sb.controller.AccountTypeController")
+@RequestMapping("account-type")
 public class AccountTypeController {
 
     @GetMapping("/all")
-    @ApiOperation(value = "Return general Response", notes = "Returns a general response when calling a controller")
+    @ApiOperation(value = "Gets all account types", notes = "Returns a list of configured accounts")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "The controller was called", response = GeneralResponse.class),
+            @ApiResponse(code = 200, message = "Account Types Returned", response = GeneralResponse.class),
             @ApiResponse(code = 400, message = "Bad Request", response = GeneralResponse.class),
-            @ApiResponse(code = 404, message = "Not found", response = GeneralResponse.class),
+            @ApiResponse(code = 404, message = "Not Found", response = GeneralResponse.class),
             @ApiResponse(code = 500, message = "Internal Server Error", response = GeneralResponse.class)})
-    public GeneralResponse<String> getAll() {return new GeneralResponse<String>( true, "No types found");}
-
+    public ResponseEntity<GeneralResponse<String>> getAll(){
+        GeneralResponse<String> response = new GeneralResponse<>(true, "No types found");
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
