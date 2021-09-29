39f65e83eb8b55ead8ac6af56fce8685781d195a
Janus Roestenburg <JanusRoestenburg@gmail.com>
2021-09-29 11:43:30 +0200

    Set up Account Entity

--- AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/Account.java
+++ AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/Account.java
@@ -1,4 +1,27 @@
 package za.ac.nwu.ac.domain.persistence;
 
-public class Account {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "ACCOUNT")
+public class Account implements Serializable {
+
+    @Id
+    @Column(name = "ACC_ID")
+    private long accID;
+
+    @Column(name = "ACC_BALANCE")
+    private float accBalance;
+
+    @Column(name = "ACC_START_DATE")
+    private LocalDate startDate;
+
+    @Column(name = "ACC_TYPE_ID")
+    private long typeID;
+
+    @Column(name = "MEM_ID")
+    private long memberID;
+
 }
