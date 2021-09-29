dfba12c0e4bf1ad5b1a72bcbbba0e0caf4367b82
Janus Roestenburg <JanusRoestenburg@gmail.com>
2021-09-29 11:50:42 +0200

    Added Member Entity

--- AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/AccountTransaction.java
+++ AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/AccountTransaction.java
@@ -1,4 +1,24 @@
 package za.ac.nwu.ac.domain.persistence;
 
-public class AccountTransaction {
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "MEMBER")
+public class AccountTransaction implements Serializable {
+
+    @Id
+    @Column(name = "MEM_ID")
+    private long memID;
+
+    @Column(name = "MEM_NAME")
+    private String memName;
+
+    @Column(name = "MEM_SURNAME")
+    private String memSurname;
+
+    @Column(name = "MEM_DOB")
+    private LocalDate memDOB;
 }
--- AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/Member.java
+++ AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/Member.java
@@ -1,4 +1,24 @@
 package za.ac.nwu.ac.domain.persistence;
 
-public class Member {
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "MEMBER")
+public class Member implements Serializable {
+
+    @Id
+    @Column(name = "MEM_ID")
+    private long memID;
+
+    @Column(name = "MEM_NAME")
+    private String memName;
+
+    @Column(name = "MEM_SURNAME")
+    private String memSurname;
+
+    @Column(name = "MEM_DOB")
+    private LocalDate memDOB;
 }
