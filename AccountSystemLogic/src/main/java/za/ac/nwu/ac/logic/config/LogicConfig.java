97a647f07e073e00c09434feefa0aee58ffa1950
Janus Roestenburg <JanusRoestenburg@gmail.com>
2021-10-03 10:49:22 +0200

    Linked Spring Context

--- AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/config/LogicConfig.java
+++ AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/config/LogicConfig.java
@@ -4,9 +4,9 @@ package za.ac.nwu.ac.logic.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import za.ac.nwu.ac.translator.config.TranslatorConfig;
 
-//@Import({TranslaterCofig.class})
-
+@Import({TranslatorConfig.class})
 @Configuration
 @ComponentScan(basePackages = {
         "za.ac.nwu.ac.logic.flow"
