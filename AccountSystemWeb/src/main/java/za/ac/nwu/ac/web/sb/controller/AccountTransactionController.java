72b3b60baadc391ba2ae780e5a2e739dc100f298
Janus Roestenburg <JanusRoestenburg@gmail.com>
2021-10-05 07:47:28 +0200

    Added Translator + Flow

--- AccountSystemWeb/src/main/java/za/ac/nwu/ac/web/sb/controller/AccountTransactionController.java
+++ AccountSystemWeb/src/main/java/za/ac/nwu/ac/web/sb/controller/AccountTransactionController.java
@@ -1,4 +1,58 @@
 package za.ac.nwu.ac.web.sb.controller;
 
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import za.ac.nwu.ac.domain.dto.AccountDto;
+import za.ac.nwu.ac.domain.service.GeneralResponse;
+import za.ac.nwu.ac.logic.flow.CreateAccountFlow;
+import za.ac.nwu.ac.logic.flow.FetchAccountFlow;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("account-transaction")
 public class AccountTransactionController {
+//    private final FetchAccountTransactionFlow fetchAccountTransactionFlow;
+//    private final CreateAccountTransactionFlow createAccountTransactionFlow;
+//
+//    @Autowired
+//    public AccountTransactionController(FetchAccountTransactionFlow fetchAccountTransactionFlow,@Qualifier("createAccountTransactionFlowName") CreateAccountTransactionFlow createAccountTransactionFlow) {
+//        this.fetchAccountTransactionFlow = fetchAccountTransactionFlow;
+//        this.createAccountTransactionFlow = createAccountTransactionFlow;
+//    }
+//
+//    @GetMapping("/all")
+//    @ApiOperation(value = "Gets all Account Transactions", notes = "Returns a list of account transactions")
+//    @ApiResponses(value = {
+//            @ApiResponse(code = 200, message = "Account Transactions Returned", response = GeneralResponse.class),
+//            @ApiResponse(code = 400, message = "Bad Request", response = GeneralResponse.class),
+//            @ApiResponse(code = 404, message = "Not Found", response = GeneralResponse.class),
+//            @ApiResponse(code = 500, message = "Internal Server Error", response = GeneralResponse.class)})
+//    public ResponseEntity<GeneralResponse<List<AccountTransactionDto>>> getAll(){
+//        List<AccountTransactionDto> accountTransactions = fetchAccountTransactionFlow.getAllAccountTransactions();
+//        GeneralResponse<List<AccountTransactionDto>> response = new GeneralResponse<>(true, accountTransactions);
+//        return new ResponseEntity<>(response, HttpStatus.OK);
+//    }
+//    @PostMapping("")
+//    @ApiOperation(value = "Creates a new Account Transaction", notes = "Creates a new account transaction for the account system")
+//    @ApiResponses(value = {
+//            @ApiResponse(code = 200, message = "Account Transaction was created successfully", response = GeneralResponse.class),
+//            @ApiResponse(code = 400, message = "Bad Request", response = GeneralResponse.class),
+//            @ApiResponse(code = 404, message = "Not Found", response = GeneralResponse.class),
+//            @ApiResponse(code = 500, message = "Internal Server Error", response = GeneralResponse.class)})
+//    public ResponseEntity<GeneralResponse<AccountTransactionDto>> create(
+//            @ApiParam(value = "Request body to create new Account Transaction.", required = true)
+//            @RequestBody AccountTransactionDto accountTransaction){
+//        AccountTransactionDto AccountTransactionResponse = createAccountTransactionFlow.create(accountTransaction);
+//        GeneralResponse<AccountTransactionDto> response = new GeneralResponse<>(true, AccountTransactionResponse);
+//        return new ResponseEntity<>(response, HttpStatus.CREATED);
+//    }
 }
